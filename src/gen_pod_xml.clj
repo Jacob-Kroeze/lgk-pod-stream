390accb266aaeb02be851ec4cb213c24d5bb032b
jacob kroeze <jlkroeze@gmail.com>
2026-01-26 11:38:31 -0800

    publish, revise tags in list of items (episodes)

--- src/gen_pod_xml.clj
+++ src/gen_pod_xml.clj
@@ -15,19 +15,24 @@
       ;size (io/buffer-size (io/input-stream url))
       ]
   (->> (rss/channel-xml {:title       "For Inky"
-                        :link        ch-url
-                        :description "Media for one"
-                        ;:itunes:duration    (+ (* 5 60) 51)
-                        ;:pubDate     "Sun, 25 Jan 2026 18:00:00 -0500"               ; todo formatting
-                        ;:size        size
-                        ;:type-tag "audio/mpeg"
-                        ;:enclosure {:url url :type "audio/mpeg"}
-                        ;:guid md5
-                        }
-                       [{:title     "Consider Yourself"
-                         ;:link url
-                         :enclosure [{:url url :type "audio/mp3"}]}])
-      (spit "pod.xml")))
+                         :link        ch-url
+                         :description "Media for one"
+                         ;:itunes:duration    (+ (* 5 60) 51)
+                         ;:pubDate     "Sun, 25 Jan 2026 18:00:00 -0500"               ; todo formatting
+                         ;:size        size
+                         ;:type-tag "audio/mpeg"
+                         ;:enclosure {:url url :type "audio/mpeg"}
+                         ;:guid md5
+                         }
+                        [{:title     "Consider Yourself"
+
+                          :pubDate   (java.time.Instant/now)
+                          :image     [{:url "https://commons.wikimedia.org/wiki/File:Moscow_et_Owana_de_l%E2%80%99%C3%A9levage_Of_Kolyma_Wolves.jpg?uselang=fr"}]
+                          :author    "JLK"
+                          :guid      (codecs/bytes->hex md5)
+                          :enclosure [{:url url :type "audio/mp3" :lenght (+ (* 5 60) 51)}]}])
+       (spit "pod.xml")
+       ))
 
 (comment
   (slurp "pod.xml"))
