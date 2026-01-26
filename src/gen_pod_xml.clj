fe9e56769522a3a3e00160e4879d45b32f6b8566
jacob kroeze <jlkroeze@gmail.com>
2026-01-26 11:10:20 -0800

    code to gen simple rss

---
+++ src/gen_pod_xml.clj
@@ -0,0 +1,33 @@
+(ns gen-pod-xml
+  (:require [clj-rss.core :as rss]
+            [buddy.core.hash :as hash]
+            [buddy.core.codecs :as codecs]
+            [clojure.java.io :as io]))
+
+;DatatypeConverter
+
+
+
+(let [ch-url "https://raw.githubusercontent.com/Jacob-Kroeze/lgk-pod-stream/refs/heads/main/pod.xml"
+      url "https://media-public-pod.s3.us-west-2.amazonaws.com/04-consider-yourself.mp3"
+      _ (defonce md5 (hash/md5 (io/input-stream url)))
+      ;md5 (hash/md5 (io/input-stream url))
+      ;size (io/buffer-size (io/input-stream url))
+      ]
+  (->> (rss/channel-xml {:title       "For Inky"
+                        :link        ch-url
+                        :description "Media for one"
+                        ;:itunes:duration    (+ (* 5 60) 51)
+                        ;:pubDate     "Sun, 25 Jan 2026 18:00:00 -0500"               ; todo formatting
+                        ;:size        size
+                        ;:type-tag "audio/mpeg"
+                        ;:enclosure {:url url :type "audio/mpeg"}
+                        ;:guid md5
+                        }
+                       [{:title     "Consider Yourself"
+                         ;:link url
+                         :enclosure [{:url url :type "audio/mp3"}]}])
+      (spit "pod.xml")))
+
+(comment
+  (slurp "pod.xml"))
